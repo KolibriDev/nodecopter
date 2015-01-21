7362ff8312e07ef3b19a174ca2d5d3c6eb0be736
Benedikt D Valdez <benediktvaldez@gmail.com>
2015-01-21 17:22:49 +0000

    Fix jshint errors in count_humans

--- copter_js/count_humans.js
+++ copter_js/count_humans.js
@@ -3,33 +3,33 @@ var cv = require('opencv'),
     _ = require('underscore'),
     say = require('./say'),
     path = require('path'),
-    arrayCount = 0
-_numberOfHumans = 0
-_grabImage = false,
+    arrayCount = 0,
+    _numberOfHumans = 0,
+    _grabImage = false,
     twitter;
 
-var hc_path = path.join(path.dirname(require.resolve("opencv")), '..', 'data');
-var opencvConf = path.join(hc_path, "haarcascade_frontalface_alt2.xml");
+var hcPath = path.join(path.dirname(require.resolve('opencv')), '..', 'data');
+var opencvConf = path.join(hcPath, 'haarcascade_frontalface_alt2.xml');
 
 var countHumans = function() {
     var nrOfHumans = _.chain(faceArray).countBy().pairs().max(_.last).head().value();
     say('I see ' + nrOfHumans + (nrOfHumans > 1 ? ' humans.' : ' human.')).then(function () {
-        say("Would you like to share this with some random strangers?");
-    })
+        say('Would you like to share this with some random strangers?');
+    });
     _numberOfHumans = nrOfHumans;
     _grabImage = true;
-}
+};
 var tweetImage = function(tweetIt) {
     if (tweetIt) {
-        var text = "Hi, check out " + (_numberOfHumans > 1 ? 'these ' + _numberOfHumans + ' humans I found.' : 'this human I found.');
+        var text = 'Hi, check out ' + (_numberOfHumans > 1 ? 'these ' + _numberOfHumans + ' humans I found.' : 'this human I found.');
         twitter.statusWithImage(text, 'imageTweet.jpeg');
-        say("Tweeting done.");
+        say('Tweeting done.');
     }
     else{
-        say("It was a bad picture anyway.");
+        say('It was a bad picture anyway.');
     }
 
-}
+};
 var imageProccessing = function(buffer) {
     matrix.detectObject(opencvConf, {
         ScaleDecreaseFraction: 0.2,
@@ -40,12 +40,12 @@ var imageProccessing = function(buffer) {
         faceArray[arrayCount] = matches.length;
         arrayCount = (arrayCount + 1) % 10;
 
-    })
+    });
     if (_grabImage) {
         matrix.save('imageTweet.jpeg');
-        _grabImage = false
+        _grabImage = false;
     }
-}
+};
 countHumans.tweetImage = tweetImage;
 module.exports = function(stream, _twitter) {
     stream.on('data', imageProccessing);
